a95f1ac6eaec5e3fe0d9a1747fc8e296ee78e803
Jaegeroo <christianjheggfer@gmail.com>
2025-06-18 16:56:34 +0800

    feat: add supabase

---
+++ lib/supabase/middleware.ts
@@ -0,0 +1,45 @@
+import { createServerClient } from "@supabase/ssr";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function updateSession(request: NextRequest) {
+  let supabaseResponse = NextResponse.next({
+    request,
+  });
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value }) =>
+            request.cookies.set(name, value)
+          );
+          supabaseResponse = NextResponse.next({
+            request,
+          });
+          cookiesToSet.forEach(({ name, value, options }) =>
+            supabaseResponse.cookies.set(name, value, options)
+          );
+        },
+      },
+    }
+  );
+
+  const pathname = request.nextUrl.pathname;
+  const publicPaths = ["/sign-in", "/sign-up"];
+
+  const isPublicPath = publicPaths.includes(pathname);
+
+  const { error: authError } = await supabase.auth.getUser();
+
+  // protect private route
+  if (authError && !isPublicPath) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+
+  return supabaseResponse;
+}
